ab0201b869e0e7e0bdcfb622d429ee9b0d3a785d
touyou <fujiyou1121@gmail.com>
2024-01-30 16:15:51 +0900

    layout based design

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,86 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: WeatherWidget(),
+      ),
+    );
+  }
+}
+
+class WeatherWidget extends StatelessWidget {
+  const WeatherWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: FractionallySizedBox(
+        widthFactor: 0.5,
+        child: Column(
+          children: [
+            const Spacer(),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const AspectRatio(aspectRatio: 1, child: Placeholder()),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          '** ℃',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelLarge!
+                              .copyWith(color: Colors.blue),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          '** ℃',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelLarge!
+                              .copyWith(color: Colors.red),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  const SizedBox(height: 80),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Close',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Reload',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
