d3cbb901891c4ff8ef1eb3aa35568f53e85df55d
touyou <fujiyou1121@gmail.com>
2024-02-01 15:51:05 +0900

    fix pop bug

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -6,10 +6,8 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: WeatherView(),
-      ),
+    return const Scaffold(
+      body: WeatherView(),
     );
   }
 }
