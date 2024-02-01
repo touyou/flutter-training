f4552785ef74578a000d04d19d835e666106575a
touyou <fujiyou1121@gmail.com>
2024-02-01 16:51:52 +0900

    fix lint error

--- lib/mixin/after_layout_mixin.dart
+++ lib/mixin/after_layout_mixin.dart
@@ -1,14 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 mixin AfterLayoutMixin<T extends StatefulWidget> on State<T> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.endOfFrame.then((_) {
+    unawaited(WidgetsBinding.instance.endOfFrame.then((_) {
       if (mounted) {
         afterFirstLayout();
       }
-    });
+    }));
   }
 
   void afterFirstLayout() {}
