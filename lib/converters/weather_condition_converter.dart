1bd7fa5fbb1d41741a7160119b18d47c1e291c07
touyou <fujiyou1121@gmail.com>
2024-02-02 15:59:50 +0900

    remove optional

--- lib/converters/weather_condition_converter.dart
+++ lib/converters/weather_condition_converter.dart
@@ -2,19 +2,16 @@ import 'package:flutter_training/models/weather_condition.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 class WeatherConditionConverter
-    implements JsonConverter<WeatherCondition?, String?> {
+    implements JsonConverter<WeatherCondition, String> {
   const WeatherConditionConverter();
 
   @override
-  WeatherCondition? fromJson(String? json) {
-    if (json == null) {
-      return null;
-    }
+  WeatherCondition fromJson(String json) {
     return WeatherConditionExt.fromString(json);
   }
 
   @override
-  String? toJson(WeatherCondition? object) {
-    return object?.toString().split('.').last;
+  String toJson(WeatherCondition object) {
+    return object.toString().split('.').last;
   }
 }
--- lib/models/weather.g.dart
+++ lib/models/weather.g.dart
@@ -8,8 +8,8 @@ part of 'weather.dart';
 
 _$WeatherImpl _$$WeatherImplFromJson(Map<String, dynamic> json) =>
     _$WeatherImpl(
-      weatherCondition:
-          $enumDecode(_$WeatherConditionEnumMap, json['weather_condition']),
+      weatherCondition: const WeatherConditionConverter()
+          .fromJson(json['weather_condition'] as String),
       minTemperature: json['min_temperature'] as int,
       maxTemperature: json['max_temperature'] as int,
       date: const DateTimeConverter().fromJson(json['date'] as String),
@@ -18,14 +18,8 @@ _$WeatherImpl _$$WeatherImplFromJson(Map<String, dynamic> json) =>
 Map<String, dynamic> _$$WeatherImplToJson(_$WeatherImpl instance) =>
     <String, dynamic>{
       'weather_condition':
-          _$WeatherConditionEnumMap[instance.weatherCondition]!,
+          const WeatherConditionConverter().toJson(instance.weatherCondition),
       'min_temperature': instance.minTemperature,
       'max_temperature': instance.maxTemperature,
       'date': const DateTimeConverter().toJson(instance.date),
     };
-
-const _$WeatherConditionEnumMap = {
-  WeatherCondition.sunny: 'sunny',
-  WeatherCondition.cloudy: 'cloudy',
-  WeatherCondition.rainy: 'rainy',
-};
